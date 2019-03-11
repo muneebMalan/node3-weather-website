5b7687172c7a1277eb5c1ac859b2f65ddc9c56c6
Muneeb <muneeb.malan10@gmail.com>
2019-03-11 21:40:45 +0200

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,9 @@ const forecast = (latitude, longitude, callback) => {
       }else{
 
         callback(undefined, body.daily.data[0].summary+' It is currently '
-        +body.currently.temperature+' degress out. There is a '+body.currently.precipProbability+'% chance of rain')
+        +body.currently.temperature+' degress out. The high today is ' + body.daily.data[0].temperatureHigh+' degress with a low of '
+        +body.daily.data[0].temperatureLow+' degress. There is a '+body.currently.precipProbability+'% chance of rain.'
+        )
       }
    })
 }
